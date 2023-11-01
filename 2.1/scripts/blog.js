36d5f66711d791bcb8668f30eab41e8a7c610d2f
Silbye <109812796+Silbye@users.noreply.github.com>
2023-11-01 20:16:09 +0100

    Done lesson 2

---
+++ 2.1/scripts/blog.js
@@ -0,0 +1,42 @@
+new Vue({
+    el: "#main",
+    data: {
+        articleItems: [{
+                imgUrl: "img/article-1.png",
+                subTitle: "Kitchan Design",
+                title: "Let’s Get Solution For Building Construction Work",
+                date: "26 December,2022",
+            },
+            {
+                imgUrl: "img/article-2.png",
+                subTitle: "Living Design",
+                title: "Low Cost Latest Invented Interior Designing Ideas.",
+                date: "22 December,2022",
+            },
+            {
+                imgUrl: "img/article-3.png",
+                subTitle: "Interior Design",
+                title: "Best For Any Office & Business Interior Solution",
+                date: "25 December,2022",
+            },
+            {
+                imgUrl: "img/article-4.png",
+                subTitle: "Kitchan Design",
+                title: "Let’s Get Solution For Building Construction Work",
+                date: "26 December,2022",
+            },
+            {
+                imgUrl: "img/article-5.png",
+                subTitle: "Living Design",
+                title: "Low Cost Latest Invented Interior Designing Ideas.",
+                date: "22 December,2022",
+            },
+            {
+                imgUrl: "img/article-6.png",
+                subTitle: "Interior Design",
+                title: "Best For Any Office & Business Interior Solution",
+                date: "25 December,2022",
+            },
+        ],
+    },
+});
